07c40c7e2e03d62a5bbaca3c584a31d66b8130bf
oldpilluwu <fawwazamin29@gmail.com>
2021-06-26 16:05:45 +0600

    fixed streaming links

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const AnimuGetter = require("animu-desu");
 const express = require("express");
+const scapper = require("./scrapper");
 
+const getLinks = require("./links");
+var cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+// app.use(function (req, res, next) {
+//     res.header(
+//         "Access-Control-Allow-Origin",
+//         "https://gogoanime-api-project.herokuapp.com/",
+//     ); // update to match the domain you will make the request from
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept",
+//     );
+//     next();
+// });
 
 //getting the popular animes from page 1.
 
-app.get("/popular/:page", (req, res) => {
+app.get("/popular/:page", (req, res, next) => {
     let page = req.params.page || 1;
     AnimuGetter.getPopular(page)
         .then((data) => {
@@ -51,15 +67,31 @@ app.get("/anime/:id", (req, res) => {
         .catch((err) => res.status(404).send("Error occurred: " + err));
 });
 
-app.get("/anime/:id/:episode", (req, res) => {
+app.get("/anime/:id/:episode", async (req, res) => {
     let anime = req.params.id;
     let episode = req.params.episode;
-    AnimuGetter.getEpisodeLinks(anime, episode)
+    getLinks
+        .getEpisodeLink(anime, episode)
         .then((data) => {
+            console.log(data);
             res.send(data);
         })
-        .catch((err) => res.status(404).send("Error occurred: " + err));
+        .catch((err) => {
+            console.log(err);
+            res.status(404).send("Error occurred: " + err);
+        });
 });
+// app.get("/anime/:id/:episode", async (req, res) => {
+//     let anime = req.params.id;
+//     let episode = req.params.episode;
+//     try {
+//         const result = await scapper.watchAnime("naruto-episode-1");
+//         res.header("Content-Type", "application/json");
+//         res.send(JSON.stringify(result, null, 4));
+//     } catch (err) {
+//         res.send(err);
+//     }
+// });
 
 app.get("/", (req, res) => {
     let searchField = req.query.search;
---
+++ src/links.js
@@ -0,0 +1,93 @@
+const baseURL = "https://gogo-stream.com/videos/";
+
+("use strict");
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value);
+                  });
+        }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator["throw"](value));
+                } catch (e) {
+                    reject(e);
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected);
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next(),
+            );
+        });
+    };
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getGenreList =
+    exports.getRecentlyAdded =
+    exports.searchByGenre =
+    exports.getEpisodeLinks =
+    exports.search =
+    exports.getAnimeDetails =
+    exports.getPopular =
+        void 0;
+const cheerio_1 = __importDefault(require("cheerio"));
+const axios_1 = __importDefault(require("axios"));
+// const baseURL = "https://gogoanime.so/";
+
+function getEpisodeLink(id, episode) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let animeStreamingLinkGogo;
+        let finalLinksList = [];
+        const siteUrl = `${baseURL}${id}-episode-${episode}`;
+        yield axios_1.default
+            .get(siteUrl)
+            .then((response) =>
+                __awaiter(this, void 0, void 0, function* () {
+                    const html = response.data;
+                    try {
+                        var $ = cheerio_1.default.load(html);
+                        if ($(".entry-title").text() === "404") {
+                            throw new Error("Episode not found");
+                        }
+                        animeStreamingLinkGogo = $("iframe").attr("src");
+                        const downloadsLinkGogo =
+                            "https:" + animeStreamingLinkGogo;
+                        console.log(downloadsLinkGogo);
+                        finalLinksList.push({
+                            link: downloadsLinkGogo,
+                            quality: "Play",
+                        });
+                    } catch (error) {
+                        throw error;
+                    }
+                }),
+            )
+            .catch((error) => {
+                throw {
+                    error: error.message,
+                };
+            });
+        return finalLinksList;
+    });
+}
+exports.getEpisodeLink = getEpisodeLink;
